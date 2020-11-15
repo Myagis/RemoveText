20a91c56f23de471c428be655e8a7385c49097b8
Myagis <zekonis.julius1@gmail.com>
2020-11-15 19:49:34 +0200

    Doma

--- Program.cs
+++ Program.cs
@@ -22,55 +22,57 @@ namespace _2savarankiškas
         public static void Process(string fin, string fout, string finfo)
         {
             string[] lines = File.ReadAllLines(fin, Encoding.UTF8);
+            bool flag = true;
+            string newLine= "";
+
             using (var writerF = File.CreateText(fout))
             {
                 using (var writerI = File.CreateText(finfo))
                 {
+
                     foreach (string line in lines)
                     {
                         if (line.Length > 0)
                         {
-                            string newLine = line;
-                            if (TaskUtils.RemoveComments(line, out newLine))
+                            if (line.Contains("/*"))
                             {
-                                writerI.WriteLine(line);
+                                flag = false;
                             }
-                            if (newLine.Length > 0)
+                            if (line.Contains("*/"))
                             {
-                                writerF.WriteLine(newLine);
+                                flag = true;
                             }
-                        }
-                        else
-                        {
-                            writerF.WriteLine(line);
+
+                            if (TaskUtils.CreateString(line, newLine, flag) != "")
+                            {
+                                writerF.WriteLine(TaskUtils.CreateString(line, newLine, flag));
+                            }
                         }
                     }
                 }
             }
         }
 
-    }
+      
 
     class TaskUtils
     {
-        public static bool RemoveComments(string line, out string newLine)
-        {
-            newLine = line;
-            for (int i = 0; i < line.Length - 1; i++)
+            public static string CreateString(string line, string newLine, bool flag)
             {
-                if (line[i] == '/' && line[i + 1] == '/') 
+                if (flag && !line.Contains("*/"))
                 {
-                    newLine = line.Remove(i);
-                    return true;
-                }
-                else if(line[i] == '*' && line[i + 1] == '/')
-                {
-                    newLine = line.Substring(i+ 2);
-                    return true;
+                    newLine = line;
                 }
 
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+                    if (line[i] == '/' && line[i + 1] == '/')
+                    {
+                        newLine = line.Remove(i);
+                    }
+                }
+                return newLine;
             }
-            return false;
         }
     }
 }
